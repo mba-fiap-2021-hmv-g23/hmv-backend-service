a5d9716524e2b2fa4b13124a55f7284090c23def
Alisson Fernando Cardoso Veiga <afcveiga@outlook.com>
2022-03-21 22:24:48 -0300

    feat: :sparkles: checkin - domain entity
    adicionado enums de checkin.

---
+++ src/main/java/br/com/fiap/hmv/domain/entity/CheckInMode.java
@@ -0,0 +1,7 @@
+package br.com.fiap.hmv.domain.entity;
+
+public enum CheckInMode {
+
+    DIGITAL, ANAMNESE;
+
+}
---
+++ src/main/java/br/com/fiap/hmv/domain/entity/EstimatedTimeArrival.java
@@ -0,0 +1,10 @@
+package br.com.fiap.hmv.domain.entity;
+
+public enum EstimatedTimeArrival {
+
+    ESTA_NO_LOCAL,
+    NO_LOCAL_EM_ATE_15_MINUTOS,
+    NO_LOCAL_EM_ATE_30_MINUTOS,
+    NO_LOCAL_APOS_30_MINUTOS;
+
+}
