be6cfa94c26e2eeb97e5de18690d408b365b6417
Alisson Fernando Cardoso Veiga <afcveiga@outlook.com>
2022-03-27 07:25:40 -0300

    fix: :adhesive_bandage: checkin
    ajuste no formulário.

--- src/main/java/br/com/fiap/hmv/infra/rest/api/v1/model/CheckInFormAnswerModel.java
+++ src/main/java/br/com/fiap/hmv/infra/rest/api/v1/model/CheckInFormAnswerModel.java
@@ -2,12 +2,16 @@ package br.com.fiap.hmv.infra.rest.api.v1.model;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @ApiModel("CheckInQuestionModelV1")
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CheckInFormAnswerModel {
 
     @ApiModelProperty(value = "ID da pergunta.", required = true)
